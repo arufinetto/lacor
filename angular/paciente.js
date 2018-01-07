6ed2566a435060a7284e93bcbd78ed1e7520055a
Ani Rufinetto <anirufinetto@gmail.com>
2018-01-07 03:10:52 -0300

    Modularizacion de los controllers

---
+++ angular/paciente.js
@@ -0,0 +1,154 @@
+starter.factory("servicio", function(){
+				return {
+					data: {}
+					
+				} ;
+}).controller("pacienteController", function($scope, $http,servicio,$ngBootbox) {
+
+	$scope.pacienteSelected = null;
+	$scope.isEmpty = function(value){
+		return (value == "" || value == null);
+		};
+		
+	$scope.paciente ={}
+	$scope.newPaciente = {
+		nombre: '',
+		apellido: '',
+		fechaNacimiento: '',
+		domicilio: '',
+		tipoDocumento: '',
+		documento: '',
+		medicacion:'',
+		telefono:'',
+		celular:'',
+		email:'',
+		ciudad:'',
+		obraSocial:[]
+		
+	};
+	$scope.pacienteSelected = null;
+	$scope.pacienteList = {};
+	$scope.pacienteListFilter = {};
+	$scope.nombrePaciente= null;
+	$scope.obraSocialPacienteSelected = null;
+	$scope.pacienteEdited = null;
+	
+	$scope.removePaciente = function(id){
+		$http.delete('/api/paciente/'+id).success(function(data){
+			$scope.pacienteList=data
+		}).error(function(err){
+			$ngBootbox.alert(err)
+		})
+	}
+	
+	
+	$scope.pedidosByPacienteList = {}
+		
+	$scope.getPedidoByPaciente = function(paciente){
+	$http.get('api/pedidosByPaciente/' + paciente._id)
+		.success(function(data) {
+			$scope.pedidosByPacienteList = data; 
+			//data.listaPedidoPorPaciente = $scope.pedidosByPacienteList;
+		}).error(function(err) {
+			console.log('Error: '+err);
+		});
+	}
+	
+	$scope.validatePaciente = function(paciente){
+		$scope.getPedidoByPaciente(paciente);
+		if($scope.pedidosByPacienteList.length > 0){
+			$scope.confirmRemovePaciente(paciente)
+		}else{
+			$ngBootbox.confirm('El paciente no puede ser eliminado porque tiene pedidos.'+ '\n' +
+			'Si realmente desea eliminar el paciente, realice los siguientes pasos: ' + '\n' +
+			' 1. INVALIDE los pedidos (cualquier pedido que no se haya entregado puede ser invalidado)' + '\n' +
+			' 2. ELIMINE  los pedidos'+ '\n' +
+			' 3.VUELVA a la lista de pacientes e intente eliminar el paciente.')
+			}
+	}
+	
+	$scope.confirmRemovePaciente = function(paciente) {
+		  $ngBootbox.confirm('Realmente desea ELIMINAR el paciente: '+ paciente.apellido+', '+paciente.nombre +' ?'+  '\n\n'+'  UNA VEZ ELIMINADO NO PODRÁ SER RECUPERADO')
+			.then(function() {
+			  $scope.removePaciente(paciente._id);
+			},
+			function() {
+			  //Confirm was cancelled, don't delete customer
+			  console.log('Confirm was cancelled');
+			});
+		};
+	
+	$http.get('/api/pacientes')
+	.success(function(data) {
+		$scope.pacienteList = data;
+	})
+	.error(function(err) {
+		console.log('Error: '+err);
+	});
+	
+	
+	$scope.search = function() {
+		$http.get('/api/pacientesBy?nombre='+ $scope.nombrePaciente)
+		.success(function(data) {
+			$scope.pacienteListFilter = data;
+			console.log(data)
+		})
+		.error(function(err) {
+			console.log('Error: '+err);
+		});
+	};
+	
+	
+	$scope.createPaciente = function () {
+		$http.post('/api/paciente', $scope.newPaciente)
+		.success(function(data) {
+			$scope.pacienteList = data;
+			$scope.newPaciente = {};
+			$ngBootbox.alert("El paciente ha sido creado exitosamente!")
+		})
+		.error(function(err) {
+			console.log('Error: '+err);
+			ngBootbox.alert("Se produjo un problema, y el paciente no pude ser creado: " + err)
+		});
+	};
+	
+	$scope.agregarObraSocial = function(paciente,obraSocial,afiliado){
+		$scope.obraSocial = {"obraSocial":obraSocial,"afiliado":afiliado};
+		paciente.obraSocial.push($scope.obraSocial)
+	}
+	
+	$scope.agregarPaciente = function (paciente){
+		servicio.data.paciente = paciente
+		$scope.pacienteSelected = paciente.nombre+" "+paciente.apellido
+		$scope.obraSocialPacienteSelected = paciente.obraSocial;
+		console.log("obra social para paciente:"+paciente.obraSocial)
+	/*if(paciente.prestador !="" && paciente.afiliado != ""){
+			$scope.pacienteSelectedAfiliado = paciente.prestador+" - Numero de Afiliado: "+paciente.afiliado
+		}else{
+			if((paciente.prestador != null || paciente.prestador !=""))
+			{$scope.pacienteSelectedAfiliado = paciente.prestador}
+		}*/
+		
+	}
+	
+	$scope.updatePaciente = function (paciente) {
+		$http.put('/api/paciente/' + paciente._id, {nombre:paciente.nombre,apellido:paciente.apellido,fechaNacimiento:paciente.fechaNacimiento,ciudad:paciente.ciudad, email:paciente.email,obraSocial:paciente.obraSocial,
+		documento:paciente.documento,tipoDocumento:paciente.tipoDocumento,telefono:paciente.telefono,medicacion:paciente.medicacion,celular:paciente.celular,domicilio:paciente.domicilio})
+		.success(function(data) {
+			$scope.pacienteList = data;
+		})
+		.error(function(err) {
+			console.log('Error: '+err);
+		});
+	};
+	
+	$scope.editarItem = function (index) {
+				$scope.pacienteSelected = $scope.pacienteList[index];
+				console.log($scope.pacienteSelected.nombre);
+			};
+			
+			
+	
+			
+	
+	})
---
+++ angular/root.js
@@ -0,0 +1,72 @@
+var starter = angular.module("MainApp", ["ngRoute","ngBootbox"])
+.config(function($routeProvider){
+     $routeProvider
+			.when('/', {
+				templateUrl	: 'pages/landing.html',
+			}).when('/login', {
+				templateUrl: 'views/login.html',
+				controller: 'loginController'
+			  })
+			  .when('/signup', {
+				templateUrl: 'views/signup.html',
+				controller: 'loginController'
+			  }).when('/backup', {
+				templateUrl: 'pages/backup.html',
+				controller: 'loginController'
+			  }). when("/resultados", {
+                controller: "pedidoController",
+                templateUrl: "resultados.html"
+            }).when("/metodos", {
+                controller: "muestraMetodoController",
+                templateUrl: "pages/metodo.html"
+            }).when("/prestadores", {
+                controller: "pedidoController",
+                templateUrl: "pages/prestador.html"
+            }).when("/medicos", {
+                controller: "medicoController",
+                templateUrl: "pages/medicos.html"
+            }).when("/pedido", {
+                controller: "pedidoController",
+                templateUrl: "pages/pedido.html"
+            }).when("/pedidos-invalidos",{
+				templateUrl :"pages/pedidos-invalidos.html"
+			})
+			.when("/muestras", {
+                controller: "muestraMetodoController",
+                templateUrl: "pages/muestra.html"
+			}).when("/localidades", {
+                controller: "muestraMetodoController",
+                templateUrl: "pages/localidad.html"
+            }).when("/perfil",{
+				controller: "perfilController",
+                templateUrl: "pages/perfil.html"
+			}).when("/nuevo-perfil",{
+				controller: "perfilController",
+                templateUrl: "pages/nuevo-perfil.html"
+			}).when("/pedidos-entregar", {
+                controller: "pedidoController",
+                templateUrl: "pages/pedidos-entregar.html"
+            }).when("/pedidos-entregados", {
+                controller: "pedidoController",
+                templateUrl: "pages/pedidos-entregados.html"
+            })
+            .when("/analysis-list", {
+                controller: "analisisController",
+                templateUrl: "pages/analysis-list.html"
+            }).when("/paciente", {
+                controller: "pacienteController",
+                templateUrl: "pages/paciente.html"
+            }).when("/paciente-list", {
+                controller: "pacienteController",
+                templateUrl: "pages/paciente-list.html"
+            }).when("/pedidos-creados", {
+				controller: "pedidoController",
+                templateUrl: "pages/pedidos-creados.html"
+            }).when("/pedido-list",{
+				controller: "pedidoController",
+				templateUrl: "pages/pedido-list.html"
+			}).when("/save-results",{
+				controller: "pedidoController",
+				templateUrl: "pages/save-results.html"
+			})
+})
