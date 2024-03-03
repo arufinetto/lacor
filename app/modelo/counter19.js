3b024a0cf2361b8da565ad2ddec6e21774859f91
Ani Rufinetto <anirufinetto@MacBook-Pro.local>
2024-03-03 03:10:04 -0300

    load automatically results

---
+++ app/modelo/counter19.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var counter19Schema = new Schema({
+   originalResult:{type:String},
+   parsedResult:{ type: String},
+   patientId:{type: String},
+   date:{type: String}
+
+	
+});
+
+module.exports = mongoose.model("counter19", counter19Schema);
--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ var jwt = require('jsonwebtoken');
 });*/
 
 var verifyToken = function (req, res, next) {
-/* const authHeader = req.headers['authorization']
+  const authHeader = req.headers['authorization']
   if(typeof authHeader !== 'undefined'){
     const token = authHeader.split(' ')
    jwt.verify(token[1],"MY_SECRET",(err, authData) => {
@@ -32,7 +32,7 @@ var verifyToken = function (req, res, next) {
     });
   }else{
     return res.status(403).send({"message":'Forbidden'});
-  }*/
+  }
 }
 
 /*var expiredToken = function(req, res){
@@ -65,7 +65,9 @@ module.exports = function(app,passport) {
 
 	app.get('/api/excel', ControllerAnalisis.convertExcelToJson);
 
-  app.get('/api/analisis', ControllerAnalisis.findAllAnalysisWithoutPag)
+  	app.get('/api/analisis', ControllerAnalisis.findAllAnalysisWithoutPag)
+
+  	app.post('/api/analisis/:id/load-cm200', ControllerPedidos.loadAutomaticResult)
 
 
 	/**Fin Controler Analisis**/
@@ -98,7 +100,7 @@ module.exports = function(app,passport) {
 
 	app.delete('/api/paciente/:id', ControllerPacientes.deletePaciente);
 
-  app.get('/api/paciente/:id',ControllerPacientes.findPacienteById);
+    app.get('/api/paciente/:id',ControllerPacientes.findPacienteById);
 
 	app.post('/api/paciente', ControllerPacientes.add);
 
@@ -126,7 +128,7 @@ module.exports = function(app,passport) {
 
 	app.get('/api/pedidos', ControllerPedidos.findAll);
 
-	app.get('/api/pedidosBy' , ControllerPedidos.filter);
+	app.get('/api/pedidosBy', ControllerPedidos.filter);
 
   app.get('/api/pedidos/open', ControllerPedidos.findAllOpens);
 
@@ -171,6 +173,9 @@ module.exports = function(app,passport) {
 	app.get('/api/pedidos/proporcion-estudios-derivados', ControllerPedidos.proporcionEstudiosDerivados)
 
 	app.get('/api/pedidos-estadisticas', ControllerPedidos.nuevosPedidos)
+
+	app.put('/api/pedidos/:id/update-identifier', ControllerPedidos.updateIdentifier);
+
   app.get('/api/send-sms/:phoneNumber/protocol/:protocolo/patient/:patientName/message/:message', ControllerPedidos.sendSMS);
 	/**fin controler pedido**/
 
@@ -198,13 +203,13 @@ module.exports = function(app,passport) {
 	//app.get('/api/gasto-motivo/:id', ControllerGasto.agruparGastoPorMotivo);
 
 	/**Controller Payment**/
-  app.post('/api/payment', ControllerPayment.addPayment);
-  app.put('/api/payment/:id', ControllerPayment.updatePayment);
-  app.get('/api/payment/:id', ControllerPayment.findPaymentByOrder);
+  app.post('/api/payment', verifyToken, ControllerPayment.addPayment);
+  app.put('/api/payment/:id', verifyToken, ControllerPayment.updatePayment);
+  app.get('/api/payment/:id', verifyToken, ControllerPayment.findPaymentByOrder);
 
-  app.post('/api/payment-health-insurance', ControllerPayment.addPaymentHealthInsurance);
-  app.put('/api/payment-health-insurance/:id', ControllerPayment.updatePaymentHealthInsurance);
-  app.get('/api/payment-health-insurance/:id', ControllerPayment.findPaymentHealthInsuranceByOrder);
+  app.post('/api/payment-health-insurance', verifyToken, ControllerPayment.addPaymentHealthInsurance);
+  app.put('/api/payment-health-insurance/:id', verifyToken, ControllerPayment.updatePaymentHealthInsurance);
+  app.get('/api/payment-health-insurance/:id', verifyToken, ControllerPayment.findPaymentHealthInsuranceByOrder);
 
   /*Controller SMS*/
   app.get('/api/sent-sms/:protocolo', ControllerSms.findSentSMSbyProtocol);
@@ -214,4 +219,7 @@ module.exports = function(app,passport) {
   app.get('/api/sms', ControllerSms.process);
   app.get('/api/sms-text', ControllerSms.getSmsText);
   app.put('/api/sms-text', ControllerSms.updateSmsText);
+
+  app.post('/api/save-result/counter19', ControllerPedidos.saveResultCounter19);
+  app.get('/api/result/counter19/patient/:id', ControllerPedidos.getAutomaticResultCounter19);
 };
